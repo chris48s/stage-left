fadc121dc2ac0b129f75e64752d2947e5be13aa2
chris48s <chris.shaw480@gmail.com>
2022-08-31 21:02:45 +0100

    add (failing) test suite for items and groups

---
+++ tests/test_groups.py
@@ -0,0 +1,134 @@
+import pytest
+
+from stage_left.parser import parse
+from stage_left.types import Group, Item, ParseError, State
+
+valid_groups = """
+[ ] Item 1 of group
+[ ] Item 2 of group
+
+[ ] Item of another group
+
+Todos
+[ ] Item 1
+[ ] Item 2
+
+Group 1
+[ ] Item
+         
+Group 2
+[ ] Item
+
+Empty Group
+"""  # noqa:W293
+
+leading_whitespace1 = """ Todos
+[ ] Do this
+"""
+
+leading_whitespace2 = """    Todos
+[ ] Do this
+"""
+
+opening_square_bracket = """[Todos]
+[ ] Do this
+"""
+
+no_blank_line = """[ ] Do this
+Todos
+[ ] Do this
+"""
+
+invalid_groups = [
+    leading_whitespace1,
+    leading_whitespace2,
+    opening_square_bracket,
+    no_blank_line,
+]
+
+
+def test_groups_valid():
+    expected = [
+        Group(
+            items=[
+                Item(
+                    state=State.OPEN,
+                    description="Item 1 of group",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                ),
+                Item(
+                    State.OPEN,
+                    description="Item 2 of group",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                ),
+            ],
+            title="",
+        ),
+        Group(
+            items=[
+                Item(
+                    State.OPEN,
+                    description="Item of another group",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                )
+            ],
+            title="",
+        ),
+        Group(
+            items=[
+                Item(
+                    State.OPEN,
+                    description="Item 1",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                ),
+                Item(
+                    State.OPEN,
+                    description="Item 2",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                ),
+            ],
+            title="Todos",
+        ),
+        Group(
+            items=[
+                Item(
+                    State.OPEN,
+                    description="Item",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                )
+            ],
+            title="Group 1",
+        ),
+        Group(
+            items=[
+                Item(
+                    State.OPEN,
+                    description="Item",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                )
+            ],
+            title="Group 2",
+        ),
+        Group(items=[], title="Empty Group"),
+    ]
+    assert parse(valid_groups) == expected
+
+
+@pytest.mark.parametrize("group", invalid_groups)
+def test_groups_invalid(group):
+    with pytest.raises(ParseError):
+        parse(group)
---
+++ tests/test_items.py
@@ -0,0 +1,186 @@
+import pytest
+
+from stage_left.parser import parse
+from stage_left.types import Group, Item, ParseError, State
+
+valid_items = """[ ] Open
+[x] Checked
+[@] Ongoing
+[~] Obsolete
+[ ] Do this
+[ ]   Do this
+[ ]
+[ ] 
+[ ]       
+[ ] This is a longer ...
+    description text
+[ ] And this one ...
+    is even ...
+    longer
+[ ] The following is just ...
+    [ ] description text
+[x] These lines ...
+    should all ...
+    look the same
+[ ] This has some ...
+       more spaces
+[ ] And this one ...
+             as well
+"""  # noqa:W291
+
+no_indent = """[ ] The next line is ...
+invalid"""
+
+insufficient_indent1 = """[ ] The next line is ...
+ invalid"""
+
+insufficient_indent2 = """[ ] The next line is ...
+   invalid"""
+
+tab_indent = """[ ] The next line is ...
+	invalid (it’s a tab)"""  # noqa:W191,E101
+
+invalid_items = [
+    "[*] Invalid",
+    "[o] Invalid",
+    "[X] Invalid (uppercase)",
+    "[ ] Invalid (non-breaking space)",
+    "[] Invalid",
+    "[  ] Invalid",
+    "[ x ] Invalid",
+    "[@@] Invalid",
+    " [x] Invalid",
+    "    [x] Invalid",
+    "[ ]Invalid",
+    "[ ]! Invalid",
+    "[ ]. Invalid",
+    "[ ]!!. Invalid",
+    "[ ]#invalid",
+    "[ ]->2022-02-16 Invalid",
+    no_indent,
+    insufficient_indent1,
+    insufficient_indent2,
+    tab_indent,
+]
+
+
+def test_items_valid():
+    expected = [
+        Group(
+            items=[
+                Item(
+                    state=State.OPEN,
+                    description="Open",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                ),
+                Item(
+                    state=State.CHECKED,
+                    description="Checked",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                ),
+                Item(
+                    state=State.ONGOING,
+                    description="Ongoing",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                ),
+                Item(
+                    state=State.OBSOLETE,
+                    description="Obsolete",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                ),
+                Item(
+                    state=State.OPEN,
+                    description="Do this",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                ),
+                Item(
+                    state=State.OPEN,
+                    description="  Do this",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                ),
+                Item(
+                    state=State.OPEN,
+                    description="",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                ),
+                Item(
+                    state=State.OPEN,
+                    description="",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                ),
+                Item(
+                    state=State.OPEN,
+                    description="      ",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                ),
+                Item(
+                    state=State.OPEN,
+                    description="This is a longer ...\ndescription text",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                ),
+                Item(
+                    state=State.OPEN,
+                    description="And this one ...\nis even ...\nlonger",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                ),
+                Item(
+                    state=State.OPEN,
+                    description="The following is just ...\n[ ] description text",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                ),
+                Item(
+                    state=State.CHECKED,
+                    description="These lines ...\nshould all ...\nlook the same",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                ),
+                Item(
+                    state=State.OPEN,
+                    description="This has some ...\n   more spaces",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                ),
+                Item(
+                    state=State.OPEN,
+                    description="And this one ...\n         as well",
+                    tags=[],
+                    priority=None,
+                    due_date=None,
+                ),
+            ],
+            title="",
+        )
+    ]
+    assert parse(valid_items) == expected
+
+
+@pytest.mark.parametrize("item", invalid_items)
+def test_items_invalid(item):
+    with pytest.raises(ParseError):
+        parse(item)
